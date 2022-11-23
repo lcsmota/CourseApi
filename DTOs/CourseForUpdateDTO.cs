62079b19925f48cb7492d5369ee76ee89942c5b0
lcsmota <sacul_m@protonmail.com>
2022-11-23 08:27:19 -0300

    Add Course DTO for update

---
+++ DTOs/CourseForUpdateDTO.cs
@@ -0,0 +1,8 @@
+namespace CourseApi.DTOs;
+public class CourseForUpdateDTO
+{
+    public string Turno { get; set; }
+    public short CargaHoraria { get; set; }
+    public string Observacao { get; set; }
+    public bool EOnline { get; set; }
+}
