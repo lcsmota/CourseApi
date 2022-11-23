ac869c225c928129e2eaeecd06094f47ac720c2e
lcsmota <sacul_m@protonmail.com>
2022-11-23 08:19:09 -0300

    Add model classes

---
+++ Models/Course.cs
@@ -0,0 +1,14 @@
+namespace CourseApi.Models;
+public class Course
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Categoria { get; set; }
+    public string OferecidoPor { get; set; }
+    public string Descricao { get; set; }
+    public string Turno { get; set; }
+    public short CargaHoraria { get; set; }
+    public string Observacao { get; set; }
+    public bool EOnline { get; set; }
+    public List<Student> Students { get; set; } = new();
+}
---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+namespace CourseApi.Models;
+public class Student
+{
+    public int RA { get; set; }
+    public string Nome { get; set; }
+    public short Idade { get; set; }
+    public string Endereco { get; set; }
+    public string Telefone { get; set; }
+    public int CursoId { get; set; }
+}
