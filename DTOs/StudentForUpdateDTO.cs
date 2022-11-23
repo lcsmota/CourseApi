edfa58e78bb6a71106669dc88447bce90c13272c
lcsmota <sacul_m@protonmail.com>
2022-11-23 08:26:56 -0300

    Add Student DTO for update

---
+++ DTOs/StudentForUpdateDTO.cs
@@ -0,0 +1,8 @@
+namespace CourseApi.DTOs;
+public class StudentForUpdateDTO
+{
+    public string Nome { get; set; }
+    public short Idade { get; set; }
+    public string Endereco { get; set; }
+    public string Telefone { get; set; }
+}
